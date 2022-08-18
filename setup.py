9ae10bf73e427dd25bf80468e85daf717683ce65
teddylee777 <teddylee777@gmail.com>
2022-08-19 02:41:29 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datalist',
+    version='0.0.1',
+    description='Sample Datasets for Beginners',
+    author='Teddy Lee',
+    author_email='teddylee777@gmail.com',
+    url='https://github.com/teddylee777/datalist',
+    install_requires=['tqdm', 'pandas', 'jupyter', 'ipywidgets', 'requests'],
+    packages=find_packages(exclude=[]),
+    keywords=['teddynote', 'datasets', 'sample-datasets', 'python', 'pandas', 'sample', 'data'],
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
