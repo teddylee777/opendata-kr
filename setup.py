da050b470dc5c5ed6d583fbaf6c89fc4f00aec36
teddylee777 <teddylee777@gmail.com>
2022-08-24 13:23:03 +0900

    change function from list_all to list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opendata-kr',
-    version='0.0.3',
+    version='0.0.4',
     description='Sample Datasets for Beginners',
     author='Teddy Lee',
     author_email='teddylee777@gmail.com',
